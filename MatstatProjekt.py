a26f9e4df2e4442b68f1d2a35ff14a03d2699b0c
karlolofvincentlindberg <130671276+karlolofvincentlindberg@users.noreply.github.com>
2023-11-19 17:59:05 +0100

    Update MatstatProjekt.py

--- MatstatProjekt.py
+++ MatstatProjekt.py
@@ -102,6 +102,10 @@ def multReg(Pb):
 Pb_ny = Pb[Pb['Year1975'] != 20]
 multReg(Pb_ny)
 
+# Multipel regression exponentiell men där vi tar bort år 20 och 25 (chernobyl)
+Pb_ny = Pb[(Pb['Year1975'] != 20)&(Pb['Year1975'] != 25)]
+multReg(Pb_ny)
+
 #Prediktion utan intervall för 2025
 C,k1,k2=multReg(Pb)
 pred_B=C*np.exp(50*k1)
@@ -117,3 +121,72 @@ pred_S=np.log(10/(C*np.exp(k2)))/k1
 print('Prediktion år blyhalt under 10mg')
 print('Blekinge: '+str(pred_B+1975))
 print('Södermanland: '+str(pred_S+1975))
+
+
+#Prediktion med intervall
+def data_intervall(pb):
+    Pb_S = Pb.loc[Pb['Lan'] == 'Södermanlands län']
+    Pb_B = Pb.loc[Pb['Lan'] == 'Blekinge län']
+
+    Y_log = np.log(Pb['Pb'].values)
+    Platser = Pb['Lan'].values
+    P = [0 if plats == 'Blekinge län' else 1 for plats in Platser]
+    T = Pb['Year1975'].values
+    X = list(zip(T, P))
+
+    x_reg = sm.add_constant(X)
+    res_mult_log = sm.OLS(Y_log, x_reg).fit()
+    print(res_mult_log.summary())
+    C, k1, k2 = np.exp(res_mult_log.params[0]), res_mult_log.params[1], res_mult_log.params[2]
+    C_se, k1_se, k2_se = np.exp(res_mult_log.bse[0]), res_mult_log.bse[1], res_mult_log.bse[2] 
+    t = np.linspace(0, 100, 200)
+    y_exp_S = C*np.exp(t*k1)*np.exp(k2)
+    y_exp_B = C*np.exp(t*k1)
+
+    df = pd.DataFrame(index = t)
+    df['Södermanland'] = y_exp_S
+    df['Blekinge'] = y_exp_B
+
+    df['Beta S'] = k1
+    df['Beta B'] = k2
+    df['Beta S - stdev'] = k1_se
+    df['Beta B - stdev'] = k2_se
+    df['Beta S - undre'] = k1 - 2*k1_se
+    df['Beta B - undre'] = k2 - 2*k2_se
+    df['Beta S - övre'] = k1 + 2*k1_se
+    df['Beta B - övre'] = k2 + 2*k2_se
+   
+    y_exp_S_undre = C*np.exp(t*df['Beta S - undre'])*np.exp(df['Beta B - undre'])
+    y_exp_B_undre = C*np.exp(t*df['Beta S - undre'])
+    y_exp_S_övre = C*np.exp(t*df['Beta S - övre'])*np.exp(df['Beta B - övre'])
+    y_exp_B_övre = C*np.exp(t*df['Beta S - övre'])
+   
+    df['Södermanland - Undre'] = y_exp_S_undre
+    df['Blekinge - Undre'] = y_exp_B_undre
+    df['Södermanland - Övre'] = y_exp_S_övre
+    df['Blekinge - Övre'] = y_exp_B_övre
+   
+    plt.scatter(Pb_S['Year1975'].values, Pb_S['Pb'].values, c='blue')
+    plt.scatter(Pb_B['Year1975'].values, Pb_B['Pb'].values, c='red')
+    plt.plot(t, y_exp_S, c='blue')
+    plt.plot(t, y_exp_B, c='red')
+    plt.plot(t, y_exp_S_undre, c='lightblue')
+    plt.plot(t, y_exp_B_undre, c='lightcoral')
+    plt.plot(t, y_exp_S_övre, c='darkblue')
+    plt.plot(t, y_exp_B_övre, c='darkred')
+    plt.xlabel("Tid (år)")
+    plt.ylabel("Bly (mg/kg mossa)")
+    plt.show()
+
+    pred_B_undre = np.log(10/C)/(k1 - 2*k1_se)
+    pred_S_undre = np.log(10/(C*np.exp(k2 - 2*k2_se)))/(k1 - 2*k1_se)
+    pred_B_övre = np.log(10/C)/(k1 + 2*k1_se)
+    pred_S_övre = np.log(10/(C*np.exp(k2 + 2*k2_se)))/(k1 + 2*k1_se)
+
+    print('Prediktion år blyhalt under 10mg')
+    print('Blekinge tidigast: '+str(pred_B_undre+1975))
+    print('Södermanland tidigast: '+str(pred_S_undre+1975))
+    print('Blekinge senast: '+str(pred_B_övre+1975))
+    print('Södermanland senast: '+str(pred_S_övre+1975))
+    
+    return df 
